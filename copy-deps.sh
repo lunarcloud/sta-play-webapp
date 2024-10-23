04589e16ce5ae1f8dcac78b7028c67847b44fcfc
Sam Sarette <samuelsarette@linux.com>
2024-10-23 17:20:40 -0400

    fix copy-deps for all platforms

--- copy-deps.sh
+++ copy-deps.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-rm -r ./lib
-mkdir -p ./lib || exit 1
+shx rm -rf ./lib
+shx mkdir ./lib || exit 1
 
 # custom element polyfill for safari
-cp ./node_modules/@ungap/custom-elements/es.js ./lib/custom-elements.js
+shx cp ./node_modules/@ungap/custom-elements/es.js ./lib/custom-elements.js
 
 # 3d model viewing
-cp ./node_modules/three/build/three.module.min.js ./lib/
-cp ./node_modules/@google/model-viewer/dist/model-viewer.min.js ./lib/
+shx cp ./node_modules/three/build/three.module.min.js ./lib/
+shx cp ./node_modules/@google/model-viewer/dist/model-viewer.min.js ./lib/
 
 # file Saving
-cp ./node_modules/jszip/dist/jszip.min.js ./lib/
+shx cp ./node_modules/jszip/dist/jszip.min.js ./lib/
 
 # database
-cp ./node_modules/idb/build/index.js ./lib/idb.js
+shx cp ./node_modules/idb/build/index.js ./lib/idb.js
